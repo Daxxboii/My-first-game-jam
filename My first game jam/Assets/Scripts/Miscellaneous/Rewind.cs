2e33a57f6f1d51d0f6daa3f189f6bf772063f8df
Daxxboii <64298070+Daxxboii@users.noreply.github.com>
2020-07-12 03:45:31 +0530

    Rewind ability implemented(Basic)

--- My first game jam/Assets/Scripts/Miscellaneous/Rewind.cs
+++ My first game jam/Assets/Scripts/Miscellaneous/Rewind.cs
@@ -4,5 +4,75 @@ using UnityEngine;
 
 public class Rewind : MonoBehaviour
 {
-   public bool Is_rewinding;
+    public bool Is_rewinding;
+    List<Vector3> positions;
+
+
+    void Awake()
+    {
+        positions = new List<Vector3>();
+    }
+
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Start_rewind();
+        }
+
+        if (Input.GetKeyUp(KeyCode.Return))
+        {
+            Stop_rewind();
+        }
+
+    }
+
+
+    void FixedUpdate()
+    {
+
+        if (Is_rewinding)
+        {
+            _Rewind();
+        }
+        else
+        {
+            Record();
+        }
+    }
+
+
+    void Record()
+    {
+        positions.Insert(0, transform.position);
+        //Debug.Log(positions[0]);
+    }
+
+    void _Rewind()
+    {
+        if (positions.Count > 0)
+        {
+            transform.position = positions[0];
+            positions.RemoveAt(0);
+        }
+        else
+        {
+            Stop_rewind();
+        }
+    }
+    void Start_rewind()
+    {
+        Is_rewinding = true;
+    }
+
+
+
+    void Stop_rewind()
+    {
+        Is_rewinding = false;
+    }
 }
+   
+
