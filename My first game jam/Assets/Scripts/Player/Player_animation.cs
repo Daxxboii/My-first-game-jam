f5aff3f3ffa92664bdf8086cab0ed37d4c78bf96
ActionKnight <ishandhoundiyal0709@gmail.com>
2020-07-22 19:11:10 +0530

    Animations Scripts
    New idle animation
    Animation Script

---
+++ My first game jam/Assets/Scripts/Player/Player_animation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_animation : MonoBehaviour
+{
+    public Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        anim.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+       
+
+
+
+        // jump
+        if (Input.GetButton("Jump") || Input.GetKey(KeyCode.UpArrow) || Input.GetKey("w"))
+        {
+            anim.SetTrigger("Jump Trigger");
+        }
+        else
+        {
+
+        }
+
+
+    }
+
+}
