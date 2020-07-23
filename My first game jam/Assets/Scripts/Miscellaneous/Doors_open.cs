16b04faba4a352405fb959565f6fdb3868a196de
Daxxboii <64298070+Daxxboii@users.noreply.github.com>
2020-07-24 03:30:36 +0530

    Door Animations setup

---
+++ My first game jam/Assets/Scripts/Miscellaneous/Doors_open.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doors_open : MonoBehaviour
+{
+    private Animator door;
+    private BoxCollider collider;
+
+    void Awake()
+    {
+        door = this.gameObject.transform.GetComponent<Animator>();
+        collider = this.gameObject.GetComponent<BoxCollider>();
+    }
+    // Update is called once per frame
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            door.SetBool("Open", true);
+            collider.enabled = false;
+        }
+    }
+}
