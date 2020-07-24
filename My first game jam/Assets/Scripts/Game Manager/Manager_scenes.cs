76aeb31153552631e8ecb9bcf19d997efe0d9f5c
Daxxboii <64298070+Daxxboii@users.noreply.github.com>
2020-07-24 18:40:33 +0530

    Menu and credits done

---
+++ My first game jam/Assets/Scripts/Game Manager/Manager_scenes.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Manager_scenes : MonoBehaviour
+{
+   
+    public void Menu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Game()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+}
