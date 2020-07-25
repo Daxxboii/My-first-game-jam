db052d749ad0efece17bf543842d205b9c619f7f
Daxxboii <64298070+Daxxboii@users.noreply.github.com>
2020-07-25 14:54:43 +0530

    Lava world 1

--- My first game jam/Assets/Scripts/Player/Player_shoot.cs
+++ My first game jam/Assets/Scripts/Player/Player_shoot.cs
@@ -5,18 +5,28 @@ using UnityEngine;
 public class Player_shoot : MonoBehaviour
 {
     public GameObject particle_system;
-    public float Range;
+    public float Range = 5f;
     public GameObject muzzle;
 
     private Enemy_health enemy_health;
     void FixedUpdate()
     {
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Range))
+        if (Input.GetMouseButton(0))
         {
+            Cast();
+        }
+    }
+
+    void Cast()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(muzzle.transform.position, muzzle.transform.TransformDirection(Vector3.forward), out hit, Range))
+        {
+
             if (hit.transform.gameObject.tag == "Enemy")
             {
                 enemy_health = hit.transform.gameObject.GetComponent<Enemy_health>();
+
 
             }
         }
